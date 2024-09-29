fe22475a614f89809b47a10f28f56d6ecda319c0
Jan Matisz <janmatisz92@gmail.com>
2024-09-29 20:41:33 +0200

    lekce01 - online hodina
    Kód obsahuje tři třídy, které jsme vytvořili při online lekci. Zkouška publikování.

--- src/Classmate.java
+++ src/Classmate.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Classmate {
+import java.time.LocalDate;
+
+public class Classmate {
+    private String name;
+    private LocalDate birthday;
+    private int grade;
+    private School school;
 }
--- src/School.java
+++ src/School.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class School {
+public class School {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
